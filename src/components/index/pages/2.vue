597d91afe01000e376cbf30806c73ac2b5fe04f7
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-11-23 17:46:44 +0800

    :sparkles: 为首页添加更多栏目

--- src/components/index/pages/2.vue
+++ src/components/index/pages/2.vue
@@ -5,5 +5,8 @@
 
 		<Divider>做过的项目</Divider>
 		<index-pages-2-projects/>
+
+		<Divider>一些其他的东西</Divider>
+		<index-pages-2-misc/>
 	</div>
 </template>
---
+++ src/components/index/pages/2/misc.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="flex <sm:(flex-col) gap-5">
+		<div class="flex-1">
+			<Panel header="有用的链接">
+				<div class="flex <sm:(flex-col) gap-2">
+					<Button as="a" class="<sm:(w-full) no-underline" href="https://status.zhazha120.cn" label="监控" severity="secondary" size="small">
+						<template #icon>
+							<div class="i-ant-design:monitor-outlined"/>
+						</template>
+					</Button>
+
+					<Button as="a" class="<sm:(w-full) no-underline" href="https://qm.qq.com/cgi-bin/qm/qr?k=J0VLGhPqQMYw8j6u4v5_-o4o-gnwA6MW&authKey=G7bGP4NM1k0MO/kH+xKVlEjk9gbR2AOXCxMRDjtrqQSDxwQvAxGdseQ1uyhj9nNb" label="粉丝群" severity="secondary" size="small">
+						<template #icon>
+							<div class="i-ant-design:usergroup-add-outlined"/>
+						</template>
+					</Button>
+				</div>
+			</Panel>
+		</div>
+
+		<div class="flex-1">
+			<Panel header="在线工具">
+				<div class="flex <sm:(flex-col) gap-2">
+
+				</div>
+			</Panel>
+		</div>
+	</div>
+</template>
