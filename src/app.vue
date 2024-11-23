9589a304f010389edbe7cfdc262e215a2cda283c
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-11-23 17:46:27 +0800

    :sparkles: 添加页脚

--- src/app.vue
+++ src/app.vue
@@ -9,5 +9,19 @@ import '~/styles/global.scss'
 		<div class="container mx-auto">
 			<NuxtPage/>
 		</div>
+
+		<div class="position-(fixed bottom-0) w-full">
+			<div class="flex justify-center p-2 bg-[#222] opacity-50 transition-(opacity duration-200) hover:opacity-100">
+				&copy; 2024 - {{ new Date().getFullYear() }}
+				<Divider class="!p-0" layout="vertical"/>
+				<Button as="a" class="no-underline !p-0" href="https://beian.miit.gov.cn" link>
+					赣 ICP 备 2022005275 号
+				</Button>
+				<Divider class="!p-0" layout="vertical"/>
+				<Button as="a" class="no-underline !p-0" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36070202001088" link>
+					赣公网安备 36070202001088 号
+				</Button>
+			</div>
+		</div>
 	</NuxtLayout>
 </template>
