1e3e7498e6b032f931a471698b0b9aa3d64581db
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-11-25 23:56:24 +0800

    :wrench: 修改主题色

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import { definePreset } from '@primeuix/styled'
 import Aura from '@primevue/themes/aura'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
@@ -28,7 +29,23 @@ export default defineNuxtConfig({
 	primevue: {
 		options: {
 			theme: {
-				preset: Aura
+				preset: definePreset(Aura, {
+					semantic: {
+						primary: {
+							50: '{zinc.50}',
+							100: '{zinc.100}',
+							200: '{zinc.200}',
+							300: '{zinc.300}',
+							400: '{zinc.400}',
+							500: '{zinc.500}',
+							600: '{zinc.600}',
+							700: '{zinc.700}',
+							800: '{zinc.800}',
+							900: '{zinc.900}',
+							950: '{zinc.950}'
+						}
+					}
+				})
 			}
 		}
 	}
