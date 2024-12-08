4dd7ee6983652ec6b3935a51a3e247e6d45b44b4
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-12-08 22:18:00 +0800

    :sparkles: :lipstick: 新增 `Naive UI` 的加载条提供者包裹, 并为内容和页脚设置 `0.5rem` 的边距

--- src/app.vue
+++ src/app.vue
@@ -21,35 +21,37 @@ const naiveTheme = computed(() => {
 				<n-message-provider>
 					<n-modal-provider>
 						<n-notification-provider>
-							<div :class="{ 'px-2': isMobile }" class="container mx-auto">
-								<NuxtPage/>
-							</div>
-
-							<div class="mt-4 position-sticky bottom-2 z-120">
-								<div class="flex justify-center">
-									<Panel :dt="{ 'header.padding': 0, 'content.padding': '0.5rem' }">
-										<div :class="{ 'leading-none': isMobile }" class="flex flex-wrap justify-center items-center">
-											<span class="text-xs">&copy; 2024 - {{ new Date().getFullYear() }}</span>
-
-											<template v-if="!isMobile">
-												<Divider :dt="{ 'vertical.margin': '0 0.5rem' }" layout="vertical"/>
-											</template>
-
-											<Button :dt="{ 'sm.padding.x': 0, 'sm.padding.y': 0 }" :fluid="isMobile" as="a" href="https://beian.miit.gov.cn" size="small" variant="link">
-												赣 ICP 备 2022005275 号
-											</Button>
-
-											<template v-if="!isMobile">
-												<Divider :dt="{ 'vertical.margin': '0 0.5rem' }" layout="vertical"/>
-											</template>
-
-											<Button :dt="{ 'sm.padding.x': 0, 'sm.padding.y': 0 }" :fluid="isMobile" as="a" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36070202001088" size="small" variant="link">
-												赣公网安备 36070202001088 号
-											</Button>
-										</div>
-									</Panel>
+							<n-loading-bar-provider>
+								<div class="container mx-auto p-2">
+									<NuxtPage/>
 								</div>
-							</div>
+
+								<div class="mt-4 p-2 position-sticky bottom-2 z-120">
+									<div class="flex justify-center">
+										<Panel :dt="{ 'header.padding': 0, 'content.padding': '0.5rem' }">
+											<div :class="{ 'leading-none': isMobile }" class="flex flex-wrap justify-center items-center">
+												<span class="text-xs">&copy; 2024 - {{ new Date().getFullYear() }}</span>
+
+												<template v-if="!isMobile">
+													<Divider :dt="{ 'vertical.margin': '0 0.5rem' }" layout="vertical"/>
+												</template>
+
+												<Button :dt="{ 'sm.padding.x': 0, 'sm.padding.y': 0 }" :fluid="isMobile" as="a" href="https://beian.miit.gov.cn" size="small" variant="link">
+													赣 ICP 备 2022005275 号
+												</Button>
+
+												<template v-if="!isMobile">
+													<Divider :dt="{ 'vertical.margin': '0 0.5rem' }" layout="vertical"/>
+												</template>
+
+												<Button :dt="{ 'sm.padding.x': 0, 'sm.padding.y': 0 }" :fluid="isMobile" as="a" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36070202001088" size="small" variant="link">
+													赣公网安备 36070202001088 号
+												</Button>
+											</div>
+										</Panel>
+									</div>
+								</div>
+							</n-loading-bar-provider>
 						</n-notification-provider>
 					</n-modal-provider>
 				</n-message-provider>
