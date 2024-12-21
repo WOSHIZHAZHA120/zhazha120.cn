662de3c7dda2ad00c7a9f7d47c7f9ab3248abca6
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-12-21 17:22:39 +0800

    :sparkles: 固定福瑞头像

--- src/modules/index/components/index/pages/1/avatar.vue
+++ src/modules/index/components/index/pages/1/avatar.vue
@@ -1,27 +1,12 @@
 <script lang="ts" setup>
-import { useElementHover } from '@vueuse/core'
 import avatar_furry from '~/assets/avatar_furry.webp'
-import avatar_main from '~/assets/avatar_main.webp'
 import isMobile from '~/shared/isMobile'
-
-const avatarContainer = ref<HTMLDivElement>()
-const isAvatarHovered = useElementHover(avatarContainer)
 </script>
 
 <template>
-	<div :class="{ 'w-full mt-20': isMobile }">
+	<div :class="{ 'w-full mt-10': isMobile }">
 		<div class="size-60 mx-auto">
-			<div ref="avatarContainer" :class="{ 'w-full': isMobile }" class="screen-center-flex">
-				<Transition mode="out-in" name="fade">
-					<template v-if="isAvatarHovered">
-						<Image :src="avatar_furry" alt="设定" image-class="size-full rounded"/>
-					</template>
-
-					<template v-else>
-						<Image :src="avatar_main" alt="头像" image-class="size-40 rounded-full"/>
-					</template>
-				</Transition>
-			</div>
+			<Image :src="avatar_furry" alt="设定" image-class="size-full rounded"/>
 		</div>
 	</div>
 </template>
