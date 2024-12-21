9949791b05eed0515f4c7a23430b9946e7be05d9
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-12-21 16:44:59 +0800

    :fire: 移除不必要的注释

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { NaiveUiResolver, PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	ssr: false,
