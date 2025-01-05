2155b107f719e61d85231dd441d1ab5b8b37fd69
渣渣120 <WOSHIZHAZHA120@qq.com>
2025-01-05 22:53:48 +0800

    :lipstick: 优化友链排版

--- src/modules/index/components/index/pages/2/friends_links.vue
+++ src/modules/index/components/index/pages/2/friends_links.vue
@@ -3,22 +3,24 @@ import friends_links from '~/shared/friends_links'
 </script>
 
 <template>
-	<div class="flex justify-center flex-wrap gap-2">
-		<template v-for="friends_link in friends_links">
-			<Button :dt="{ 'padding.x': 0, 'padding.y': 0 }" :href="friends_link.href" as="a" class="transition-opacity hover:opacity-80" text>
-				<Panel :dt="{ 'header.padding': 0, 'content-padding': '1rem' }" class="w-fit">
-					<div class="flex items-center gap-2">
-						<div class="size-12">
-							<Image :src="friends_link.icon" image-class="size-full rounded-full"/>
-						</div>
+	<div class="w-full pb-2 overflow-scroll">
+		<div class="w-max mx-auto flex justify-center gap-2">
+			<template v-for="friends_link in friends_links">
+				<Button :dt="{ 'padding.x': 0, 'padding.y': 0 }" :href="friends_link.href" as="a" class="transition-opacity hover:opacity-80" text>
+					<Panel :dt="{ 'header.padding': 0, 'content-padding': '1rem' }" class="w-fit">
+						<div class="flex items-center gap-2">
+							<div class="size-12">
+								<Image :src="friends_link.icon" image-class="size-full rounded-full"/>
+							</div>
 
-						<div class="flex flex-col">
-							<span class="text-xl fw-extrabold">{{ friends_link.name }}</span>
-							<span class="text-sm text-white text-opacity-50">{{ friends_link.description }}</span>
+							<div class="flex flex-col">
+								<span class="text-xl fw-extrabold">{{ friends_link.name }}</span>
+								<span class="text-sm text-gray">{{ friends_link.description }}</span>
+							</div>
 						</div>
-					</div>
-				</Panel>
-			</Button>
-		</template>
+					</Panel>
+				</Button>
+			</template>
+		</div>
 	</div>
 </template>
