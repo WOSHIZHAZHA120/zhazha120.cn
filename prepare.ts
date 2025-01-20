2b8a0f32d48b2ab5574261b4ced5475f2ea6e8a3
渣渣120 <WOSHIZHAZHA120@qq.com>
2025-01-20 23:22:53 +0800

    :wrench: :sparkles: 更新 `postinstall` 脚本

---
+++ prepare.ts
@@ -0,0 +1,41 @@
+import { exec } from 'node:child_process'
+
+(async () => {
+	await Promise.all([
+		new Promise<void>(resolve => {
+			exec('nuxt prepare', () => {
+				resolve()
+			})
+		}),
+		new Promise<void>(resolve => {
+			exec(`nuxt prepare --cwd=src/layers/index`, () => {
+				resolve()
+			})
+		}),
+		new Promise<void>(resolve => {
+			exec(`nuxt prepare --cwd=src/layers/support`, () => {
+				resolve()
+			})
+		}),
+		new Promise<void>(resolve => {
+			exec(`nuxt prepare --cwd=src/layers/game`, () => {
+				resolve()
+			})
+		}),
+		new Promise<void>(resolve => {
+			exec(`nuxt prepare --cwd=src/layers/furry`, () => {
+				resolve()
+			})
+		}),
+		new Promise<void>(resolve => {
+			exec(`nuxt prepare --cwd=src/layers/accuracy-calculator`, () => {
+				resolve()
+			})
+		}),
+		new Promise<void>(resolve => {
+			exec(`nuxt prepare --cwd=src/layers/blog`, () => {
+				resolve()
+			})
+		})
+	])
+})()
