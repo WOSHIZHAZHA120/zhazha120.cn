01bcfc79f37db4c3acd869dd05b8405bdbd39493
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2025-02-21 11:31:03 +0800

    :sparkles: 新增友连

--- src/layers/index/shared/friends_links.ts
+++ src/layers/index/shared/friends_links.ts
@@ -7,6 +7,7 @@ import friends_link_5 from '~/assets/friends_link_5.webp'
 import friends_link_6 from '~/assets/friends_link_6.webp'
 import friends_link_7 from '~/assets/friends_link_7.webp'
 import friends_link_8 from '~/assets/friends_link_8.webp'
+import friends_link_9 from '~/assets/friends_link_9.webp'
 import type FriendsLink from '~/types/friends_link'
 
 export default [
@@ -63,5 +64,11 @@ export default [
 		description: '嘻嘻西西CC吸吸',
 		icon: friends_link_8,
 		href: 'https://xxand.cc'
+	},
+	{
+		name: 'Dracowyn',
+		description: '愿飞龙常入你梦乡',
+		icon: friends_link_9,
+		href: 'https://dracowyn.com'
 	}
 ] satisfies FriendsLink[]
